fcd338965ef05668a5a4530a25621b95fadda26d
rinatsharifullin <58179621+rinatsharifullin@users.noreply.github.com>
2020-05-10 16:12:20 +0100

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    mySvg = $('svg').drawsvg({'reverse': 'false', 'stagger': 200, 'duration': 1000});
+    $('svg').css({stroke: 'black'});
+    $(window).scroll(function(){
+        // var position = 0.5;
+        console.log($(window).scrollTop()/($(document).height()/2 -  $(window).height()));
+        var position = ($(window).scrollTop())/($(document).height()/2 -  $(window).height());
+        mySvg.drawsvg('progress', position);
+        if(position > 1 && position < 2){
+            $('svg').css({'width': 90 / position + '%', 'left': 25 + (position*25) + '%', 'top': 80 - (position*30) + '%'});
+            // $('svg').css({'transform': 'scale(' + (1 / position)+ ')', 'left': 20 + (position*30) + '%', 'top': 80 - (position*30) + '%'});
+        }
+        
+    });
+    $('svg').hover(function(){
+        $('path').css({fill: 'goldenrod'});
+
+    }, function(){
+        $('path').css({fill: 'white'});
+    });
+});
