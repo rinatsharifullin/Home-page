39b349e511217cbe23469e3fd5e97b55cdcd4f43
rinatsharifullin <58179621+rinatsharifullin@users.noreply.github.com>
2020-05-10 17:10:08 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -10,7 +10,17 @@ $(document).ready(function(){
             $('svg').css({'width': 90 / position + '%', 'left': 25 + (position*25) + '%', 'top': 80 - (position*30) + '%'});
             // $('svg').css({'transform': 'scale(' + (1 / position)+ ')', 'left': 20 + (position*30) + '%', 'top': 80 - (position*30) + '%'});
         }
-        
+        if(position > 2){
+            $('path').css({fill: 'goldenrod'});
+        }else{
+            $('path').css({fill: 'white'});
+        }
+
+    });
+    $("#fotki").vc3dEye({
+        imagePath:"images/",// the location where you’ve put the images.
+        totalImages:9,// the number of images you have.
+        imageExtension:"jpg" // the extension of the images. Make sure all the images have same extension.
     });
     $('svg').hover(function(){
         $('path').css({fill: 'goldenrod'});
